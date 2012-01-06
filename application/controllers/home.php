85cbe76cc8969ee4545edb9c4b9433e2b7b8ac33
Seth Ryder <ryder.seth@gmail.com>
2012-01-06 16:50:10 -0500

    Captcha

---
+++ application/controllers/home.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		
+	}
